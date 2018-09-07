ae67da23fe803b3bacbdce6a708e824e05ae634f
SilverBullete <1256819071@qq.com>
2018-09-07 14:31:00 +0800

    Django

--- main/index.py
+++ main/index.py
@@ -2,76 +2,42 @@
 import requests
 import json
 import threading
-from .models import cookies
-from .models import users
-from .models import logs
+from .models import cookie
 
 lock = threading.Lock()
 
 
-def getLuckyMoney(url, qq, lucky_number):
-    try:
-        user = users.objects.get(qq=qq)
-    except:
-        return "请先输入手机号码绑定，谢谢"
+def getLuckyMoney(url, lucky_number):
     while True:
         lock.acquire()
-        if user.points <= 0:
-            lock.release()
-            return "余额不足"
-        phone = user.phone
         next_lucky = 0  # 判断下一个是否为大包
         lastResidueNum = 16
         errortimes = 0
-        cook = cookies()
+        cook = cookie()
         while lastResidueNum >= 0:
             id = cook.getNextId()
-            used_times = cookies.objects.get(id=id).used_times
-            if next_lucky == 1:
-                if used_times >= 5:
-                    user.points -= 4
-                    user.save()
-                    lock.release()
-                    return "小号已用完，下个就是大包，可手动领取，谢谢，扣除4点，余额为{points}".format(points=user.points)
-                cookie = cookies.objects.get(id=id)
-                eleme_key = cookie.eleme_key
-                url_appand = cookie.url_appand
-                changePhone(eleme_key, url_appand, phone)
-                response = hongbao(url, eleme_key, url_appand, phone)
-                cookie.used_times += 1
-                changePhone(eleme_key, url_appand, cookie.phone)
-                cookie.save()
-                try:
-                    (name, amount) = gethongbaodetail(response)
-                    user.points -= 4
-                    user.save()
-                    logs.objects.create(qq=qq,money=amount)
-                    lock.release()
-                    return "恭喜你领到{name}大包，金额{amount}元，扣除4点，余额为{points}".format(name=name, amount=amount,
-                                                                           points=user.points)
-                except:
-                    user.points -= 4
-                    user.save()
-                    lock.release()
-                    return "你可能到达每日领取上限或者已领取过此链接，下个就是大包，你可以分享给好友或者留至明天手动领取，扣除4点，余额为{points}".format(points=user.points)
-            elif next_lucky == 0:
+            used_times = cookie.objects.get(id=id).used_times
+            if next_lucky == 0:
                 if used_times >= 5:
                     lock.release()
-                    return "小号已用完，还剩{num}个就是大包，此次不扣除点数，抱歉".format(num=lucky_number - lastResidueNum)
-                cookie = cookies.objects.get(id=id)
+                    return "小号已用完，还剩{num}个就是大包，抱歉".format(num=lucky_number - lastResidueNum)
+                cookie = cookie.objects.get(id=id)
                 eleme_key = cookie.eleme_key
                 url_appand = cookie.url_appand
-                changePhone(eleme_key,url_appand,cookie.phone)
-                response = hongbao(url, eleme_key, url_appand, cookie.phone)
+                track_id = cookie.track_id
+                c = cookie.cookie
+                response = hongbao(url, eleme_key, url_appand, track_id, c, cookie.phone)
                 if response == "网址错误":
                     lock.release()
                     return "网址错误，此次不扣除点数，请换个链接再来吧"
                 try:
                     if len(json.loads(response.text)['promotion_records']) == lucky_number - 1:
                         next_lucky = 1
+                        lock.release()
+                        return '下一个就是大包，请手动领取'
                     if len(json.loads(response.text)['promotion_records']) >= lucky_number:
                         lock.release()
-                        return '大包已被领取,此次不扣除点数，请换个链接再来吧'
+                        return '大包已被领取,请换个链接再来吧'
                     if len(json.loads(response.text)['promotion_records']) == lastResidueNum:
                         print('error', id)
                         errortimes += 1
@@ -80,9 +46,9 @@ def getLuckyMoney(url, qq, lucky_number):
                         cookie.used_times += 1
                         cookie.save()
                     if errortimes == 3:
-                        #cookie.used_times = 10
-                        #cookie.save()
-                        return '链接有问题，此次不扣除点数，若确定没有问题可选择再次发送尝试'
+                        cookie.used_times = 10
+                        cookie.save()
+                        return '链接有问题，若确定没有问题可选择再次发送尝试'
                         #continue
                 except:
                     continue
@@ -97,7 +63,7 @@ def changePhone(eleme_key, url_appand, phone):
     requests.put('https://h5.ele.me/restapi/v1/weixin/' + url_appand + '/phone', json.dumps(data))
 
 
-def hongbao(url, eleme_key, url_appand, phone):
+def hongbao(url, eleme_key, url_appand, track_id, cookie, phone):
     try:
         group_sn = url.split('sn=')[1].split('&')[0]
         platform = eval(url.split('platform=')[1].split('&')[0])
@@ -112,18 +78,20 @@ def hongbao(url, eleme_key, url_appand, phone):
         "device_id": "",
         "hardware_id": "",
         "platform": platform,
-        "track_id": "undefined",
+        "track_id": track_id,
         "weixin_avatar": "",
         "weixin_username": "",
         "unionid": "fuck"
     }
-    # header = {
-    #     'origin': 'https://h5.ele.me',
-    #     'referer': 'https://h5.ele.me/hongbao/',
-    #     'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 MicroMessenger/6.5.2.501 NetType/WIFI WindowsWechat QBCore/3.43.691.400 QQBrowser/9.0.2524.400)',
-    # }
+    header = {
+        'cookie': cookie,
+        'Host': 'h5.ele.me',
+        'origin': 'https://h5.ele.me',
+        'referer': 'https://h5.ele.me/hongbao/',
+        'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 MicroMessenger/6.5.2.501 NetType/WIFI WindowsWechat QBCore/3.43.691.400 QQBrowser/9.0.2524.400)',
+    }
     request_url = 'https://h5.ele.me/restapi/marketing/promotion/weixin/' + url_appand
-    response = requests.post(request_url, json.dumps(data))
+    response = requests.post(request_url, json.dumps(data),header)
     return response
 
 
--- main/models.py
+++ main/models.py
@@ -11,6 +11,19 @@ class users(models.Model):
     updated_at = models.DateTimeField(auto_created=True)
 
 
+class cookie(models.Model):
+    id = models.AutoField
+    eleme_key = models.CharField(null=False, max_length=255)
+    url_appand = models.CharField(null=False, max_length=255)
+    track_id = models.CharField(null=False, max_length=255)
+    cookie = models.CharField(null=False, max_length=2000)
+    phone = models.CharField(null=False, max_length=11)
+    qq = models.CharField(null=False, max_length=20)
+    used_times = models.IntegerField(null=False, default=0)
+
+    def getNextId(self):
+        cook = cookie.objects.order_by('used_times', 'id').first()
+        return cook.id
 
 
 class cookies(models.Model):
@@ -20,16 +33,17 @@ class cookies(models.Model):
     phone = models.CharField(null=False, max_length=11)
     used_times = models.IntegerField(null=False, default=0)
 
-
     def getNextId(self):
         cookie = cookies.objects.order_by('used_times', 'id').first()
         return cookie.id
 
+
 class kami(models.Model):
     id = models.AutoField
-    kami = models.CharField(null=False,max_length=12)
+    kami = models.CharField(null=False, max_length=12)
     val = models.IntegerField(null=False)
-    used = models.IntegerField(null=False,default=0)
+    used = models.IntegerField(null=False, default=0)
+
 
 class logs(models.Model):
     id = models.AutoField
