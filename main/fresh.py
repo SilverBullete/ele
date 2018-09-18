252724bff0912ebca1f7ab17bc03c1d09c25489b
SilverBullete <1256819071@qq.com>
2018-09-19 00:04:11 +0800

    Django

--- main/fresh.py
+++ main/fresh.py
@@ -2,7 +2,7 @@ from .models import cookie
 def fresh():
     cooks = cookie.objects.all()
     for i in cooks:
-        if i.used_times < 10:
-            i.used_times = 0
-            i.save()
+        # if i.used_times < 10:
+        i.used_times = 0
+        i.save()
 
